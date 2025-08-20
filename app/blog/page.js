0796065a665bfdebcde649799fb85e42aa2f95c5
Raj1778 <164516744+Raj1778@users.noreply.github.com>
2025-08-20 20:22:46 +0530

    Added Blog DB

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,16 +1,21 @@
-// app/blogs/page.js
+// app/blog/page.js
 import Link from "next/link";
+import { getAllBlogs } from "@/lib/db/blog";
 
-export default async function BlogsPage() {
-  const res = await fetch("http://localhost:3000/api/blogs");
-  const blogs = await res.json();
+export default async function BlogPage() {
+  const blogs = await getAllBlogs(); // Direct DB fetch
 
   return (
-    <div>
-      <h1>All Blogs</h1>
+    <div className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold mb-6">All Blogs</h1>
       {blogs.map((blog) => (
-        <div key={blog._id}>
-          <Link href={`/blogs/${blog.slug}`}>{blog.title}</Link>
+        <div key={blog._id} className="mb-4">
+          <Link
+            href={`/blog/${blog.slug}`}
+            className="text-blue-600 hover:underline"
+          >
+            {blog.title}
+          </Link>
         </div>
       ))}
     </div>
